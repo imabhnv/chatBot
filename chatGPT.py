8ac56126833820502753e9b4d88a416eb2bb9b97
Abhinav Varshney <115035256+imabhnv@users.noreply.github.com>
2024-07-16 23:37:20 +0530

    Update chatGPT.py

--- chatGPT.py
+++ chatGPT.py
@@ -1,37 +1,7 @@
-# import streamlit as st
-# from groq import Groq
-# from api import api_key
-
-# client = Groq(api_key=api_key)
-
-# st.title("A chatGPT like App") 
-
-# user_question = st.text_input("Ask me anything") 
-
-# message = {
-#     "content": f"{user_question}",
-#     "role": "user"
-# }
-
-# if st.button("ASK", use_container_width=400):
-#     try:
-#         chat_completion = client.chat.completions.create(
-#             messages=[message],
-#             model="llama3-8b-8192",
-#         )
-#         if chat_completion.choices:
-#             content = chat_completion.choices[0].message.content
-#             st.write(content)
-#         else:
-#             st.write("Error Occured")
-#     except Exception as e:
-#         st.write(f"Error occurred: {e}")
-
 import streamlit as st
 from groq import Groq
-from api import api_key
 
-client = Groq(api_key=api_key)
+client = Groq(api_key="GROQ_API_KEY")
 
 st.title("A chatGPT like App")
 
